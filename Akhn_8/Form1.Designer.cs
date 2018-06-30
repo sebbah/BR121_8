4c2ebace563e1e7a9fe98643e57cfdbb30819469
sebbah <sebbah@tlen.pl>
2018-06-30 09:55:32 +0200

    first commit

---
+++ Akhn_8/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Akhn_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(173, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(315, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "BackupM 20160805 1258.sql";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(8, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "nazwa pliku z danymi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(8, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "od numeru";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox3.Location = new System.Drawing.Point(94, 43);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(97, 26);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "02066575";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox4.Location = new System.Drawing.Point(280, 42);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(97, 26);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Text = "02066586";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox5.Location = new System.Drawing.Point(62, 72);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(33, 26);
+            this.textBox5.TabIndex = 7;
+            this.textBox5.Text = "2";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox6.Location = new System.Drawing.Point(61, 100);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(71, 26);
+            this.textBox6.TabIndex = 10;
+            this.textBox6.Text = "GOOD";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox8.Location = new System.Drawing.Point(417, 138);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(71, 21);
+            this.textBox8.TabIndex = 8;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox11.Location = new System.Drawing.Point(276, 100);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(98, 26);
+            this.textBox11.TabIndex = 11;
+            this.textBox11.Text = "No index 1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(13, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 27);
+            this.button1.TabIndex = 51;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(192, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 20);
+            this.label4.TabIndex = 52;
+            this.label4.Text = "do numeru";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(8, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 20);
+            this.label5.TabIndex = 53;
+            this.label5.Text = "dysza";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(352, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 15);
+            this.label6.TabIndex = 54;
+            this.label6.Text = "rekordów:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 166);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(477, 384);
+            this.richTextBox1.TabIndex = 55;
+            this.richTextBox1.Text = "";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(408, 48);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 56;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(8, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.TabIndex = 57;
+            this.label2.Text = "status";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(147, 103);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(129, 20);
+            this.label7.TabIndex = 58;
+            this.label7.Text = "nazwa procedury";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(101, 78);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(327, 15);
+            this.label8.TabIndex = 59;
+            this.label8.Text = "DOMYŚLNIE: 2 dla grupy 02, 3 dla grupy 03, 6 dla grupy 01";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(173, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 27);
+            this.button2.TabIndex = 60;
+            this.button2.Text = "Wyczyść";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 562);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox11);
+            this.Name = "Form1";
+            this.Text = "Akhn_8";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Akhn_8/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Akhn_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nazwaPlikuWejsciowego = textBox1.Text;
+
+            int k1 = 0;
+            int k1p = 0;
+            int k2 = 0;
+            int k2p = 0;
+            int k3 = 0;
+            int k3p = 0;
+            int k4 = 0;
+            int k4p = 0;
+            int k5 = 0;
+            int k5p = 0;
+            int k6 = 0;
+            int k6p = 0;
+            int k7 = 0;
+            int k7p = 0;
+            int k8 = 0;
+            int k8p = 0;
+            int k9 = 0;
+            int k9p = 0;
+            int k10 = 0;
+            int k10p = 0;
+            int k13 = 0;
+            int k13p = 0;
+            int k14 = 0;
+            int k14p = 0;
+            int k15 = 0;
+            int k15p = 0;
+            int k16 = 0;
+            int k16p = 0;
+            int k17 = 0;
+            int k17p = 0;
+            int k18 = 0;
+            int k18p = 0;
+            int k19 = 0;
+            int k19p = 0;
+            int k20 = 0;
+            int k20p = 0;
+            int k21 = 0;
+            int k21p = 0;
+            int k22 = 0;
+            int k22p = 0;
+            int suma = 0;
+            
+            int counter = 0;
+            int rekord; // nr zestawu danych w linii
+            string linia; // ciag znakow do analizy
+            char znak = ','; // znak rozdzielajacy dane
+            char znak2 = '\''; // char 39 - znak ktory pomijamy '
+
+            string nozzle, windex, serial, concl, testname; // dane otrzymane po analizie linii
+
+            // parametry uzytkownika
+            string dysza = textBox5.Text;
+            string procedura = textBox11.Text;
+            string status = textBox6.Text;
+            string odnr = textBox3.Text;
+            string donr = textBox4.Text;
+
+            // Read the file it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@nazwaPlikuWejsciowego);
+            
+            while ((linia = file.ReadLine()) != null)
+            {
+                counter++;
+                textBox8.Text = counter.ToString(); // ? nie odswieza?
+                rekord = 1;
+                nozzle = "";
+                windex = "";
+                serial = "";
+                concl = "";
+                testname = "";
+
+                for (int i = 0; i <= linia.Length - 1; i++)
+                {
+                    if (linia[i] == znak) rekord++;
+
+                    // `ProdN`,`POS`,`Tnum`,`TType`,`dP`,`Tx1`,`Tx2`,`T1`,`H1`,`P2`,`dPx0`,`Nozzle`,`MUTimp`,`TTime`,`PulseT`,`PulseR`,`Vref`,`Vmut`,`Error`,`Cerror`,`wIndex`,`Concl`,`Reason`,`Serial`,`Protocol`,`Year`,`MUT`,`Flow`,`Test_name`,`T_id`,`dP_ave`
+                    if (rekord == 12) if (linia[i] != znak) nozzle = nozzle + linia[i]; // Nozzle
+                    if (rekord == 21) if (linia[i] != znak) windex = windex + linia[i]; // wIndex
+                    if (rekord == 22) if (linia[i] != znak && linia[i] != znak2) concl = concl + linia[i]; // Concl
+                    if (rekord == 24) if (linia[i] != znak && linia[i] != znak2) serial = serial + linia[i]; // Serial
+                    if (rekord == 29) if (linia[i] != znak && linia[i] != znak2) testname = testname + linia[i]; // Test_name
+                }
+
+                if (concl == status && testname == procedura && nozzle == dysza)
+                    if (Int32.Parse(serial) >= Int32.Parse(odnr) && Int32.Parse(serial) <= Int32.Parse(donr))
+                    {
+                            if (windex == "1")
+                                k1++;
+                            if (windex == "1.5")
+                                k1p++;
+                            if (windex == "2")
+                                k2++;
+                            if (windex == "2.5")
+                                k2p++;
+                            if (windex == "3")
+                                k3++;
+                            if (windex == "3.5")
+                                k3p++;
+                            if (windex == "4")
+                                k4++;
+                            if (windex == "4.5")
+                                k4p++;
+                            if (windex == "5")
+                                k5++;
+                            if (windex == "5.5")
+                                k5p++;
+                            if (windex == "6")
+                                k6++;
+                            if (windex == "6.5")
+                                k6p++;
+                            if (windex == "7")
+                                k7++;
+                            if (windex == "7.5")
+                                k7p++;
+                            if (windex == "8")
+                                k8++;
+                            if (windex == "8.5")
+                                k8p++;
+                            if (windex == "9")
+                                k9++;
+                            if (windex == "9.5")
+                                k9p++;
+                            if (windex == "10")
+                                k10++;
+                            if (windex == "10.5")
+                                k10p++;
+                            if (windex == "13")
+                                k13++;
+                            if (windex == "13.5")
+                                k13p++;
+                            if (windex == "14")
+                                k14++;
+                            if (windex == "14.5")
+                                k14p++;
+                            if (windex == "15")
+                                k15++;
+                            if (windex == "15.5")
+                                k15p++;
+                            if (windex == "16")
+                                k16++;
+                            if (windex == "16.5")
+                                k16p++;
+                            if (windex == "17")
+                                k17++;
+                            if (windex == "17.5")
+                                k17p++;
+                            if (windex == "18")
+                                k18++;
+                            if (windex == "18.5")
+                                k18p++;
+                            if (windex == "19")
+                                k19++;
+                            if (windex == "19.5")
+                                k19p++;
+                            if (windex == "20")
+                                k20++;
+                            if (windex == "20.5")
+                                k20p++;
+                            if (windex == "21")
+                                k21++;
+                            if (windex == "21.5")
+                                k21p++;
+                            if (windex == "22")
+                                k22++;
+                            if (windex == "22.5")
+                                k22p++;
+                    }
+            }
+            file.Close();
+            
+            richTextBox1.Text += "GEAR\tPCS" + "\n";
+            richTextBox1.Text += "1,0\t" + k1.ToString() + "\n";
+            richTextBox1.Text += "1,5\t" + k1p.ToString() + "\n";
+            richTextBox1.Text += "2,0\t" + k2.ToString() + "\n";
+            richTextBox1.Text += "2,5\t" + k2p.ToString() + "\n";
+            richTextBox1.Text += "3,0\t" + k3.ToString() + "\n";
+            richTextBox1.Text += "3,5\t" + k3p.ToString() + "\n";
+            richTextBox1.Text += "4,0\t" + k4.ToString() + "\n";
+            richTextBox1.Text += "4,5\t" + k4p.ToString() + "\n";
+            richTextBox1.Text += "5,0\t" + k5.ToString() + "\n";
+            richTextBox1.Text += "5,5\t" + k5p.ToString() + "\n";
+            richTextBox1.Text += "6,0\t" + k6.ToString() + "\n";
+            richTextBox1.Text += "6,5\t" + k6p.ToString() + "\n";
+            richTextBox1.Text += "7,0\t" + k7.ToString() + "\n";
+            richTextBox1.Text += "7,5\t" + k7p.ToString() + "\n";
+            richTextBox1.Text += "8,0\t" + k8.ToString() + "\n";
+            richTextBox1.Text += "8,5\t" + k8p.ToString() + "\n";
+            richTextBox1.Text += "9,0\t" + k9.ToString() + "\n";
+            richTextBox1.Text += "10,0\t" + k10.ToString() + "\n";
+            richTextBox1.Text += "10,5\t" + k10p.ToString() + "\n";
+            richTextBox1.Text += "13,0\t" + k13.ToString() + "\n";
+            richTextBox1.Text += "13,5\t" + k13p.ToString() + "\n";
+            richTextBox1.Text += "14,0\t" + k14.ToString() + "\n";
+            richTextBox1.Text += "14,5\t" + k14p.ToString() + "\n";
+            richTextBox1.Text += "15,0\t" + k15.ToString() + "\n";
+            richTextBox1.Text += "15,5\t" + k15p.ToString() + "\n";
+            richTextBox1.Text += "16,0\t" + k16.ToString() + "\n";
+            richTextBox1.Text += "16,5\t" + k16p.ToString() + "\n";
+            richTextBox1.Text += "17,0\t" + k17.ToString() + "\n";
+            richTextBox1.Text += "17,5\t" + k17p.ToString() + "\n";
+            richTextBox1.Text += "18,0\t" + k18.ToString() + "\n";
+            richTextBox1.Text += "18,5\t" + k18p.ToString() + "\n";
+            richTextBox1.Text += "19,0\t" + k19.ToString() + "\n";
+            richTextBox1.Text += "19,5\t" + k19p.ToString() + "\n";
+            richTextBox1.Text += "20,0\t" + k20.ToString() + "\n";
+            richTextBox1.Text += "20,5\t" + k20p.ToString() + "\n";
+            richTextBox1.Text += "21,0\t" + k21.ToString() + "\n";
+            richTextBox1.Text += "21,5\t" + k21p.ToString() + "\n";
+            richTextBox1.Text += "22,0\t" + k22.ToString() + "\n";
+            richTextBox1.Text += "22,5\t" + k22p.ToString() + "\n";
+
+            suma = k1 + k1p + k2 + k2p + k3 + k3p + k4 + k4p;
+            suma = suma + k5 + k5p + k6 + k6p + k7 + k7p;
+            suma = suma + k13 + k13p + k14 + k14p + k15 + k15p + k16 + k16p;
+            suma = suma + k17 + k17p + k18 + k18p + k19 + k19p + k20 + k20p;
+            suma = suma + k21 + k21p + k22 + k22p;
+            richTextBox1.Text += "SUMA:\t" + suma.ToString() + "\n";
+            
+            //System.Console.WriteLine("There were {0} lines.", counter);
+            // Suspend the screen.  
+            //System.Console.ReadLine();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+    }
+}
